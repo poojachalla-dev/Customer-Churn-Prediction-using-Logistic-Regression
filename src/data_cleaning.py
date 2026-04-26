4fd45a57a00c0b48af6067e959e8ccc938e765c7
poojachalla-dev <pooja.rjch@gmail.com>
2026-04-26 15:02:29 +0530

    Converted the Churn data into 0's and 1's

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,3 +4,7 @@ from reading_data import df
 df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
 df.dropna(subset=["TotalCharges"], inplace=True)
 
+# This line converts the target column Churn from text labels into numeric values:
+df["Churn"] = df["Churn"].map({"Yes":1,"No":0})
+
+print(f"Churn:", df["Churn"])
