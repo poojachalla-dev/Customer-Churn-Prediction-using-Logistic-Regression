e561a8c99acb9fb9e2983a81b28e88c9abdb0c1d
poojachalla-dev <pooja.rjch@gmail.com>
2026-04-27 07:17:40 +0530

    Refactored the code and added eda file

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,17 +1,13 @@
 from libraries import pd
-from reading_data import df, file_name
 
 def cleaned_data(df):
 
     df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
     df.dropna(subset=["TotalCharges"], inplace=True)
 
-    # Save back to same file
-    df.to_csv(file_name, index=False)
-
     # This line converts the target column Churn from text labels into numeric values:
     df["Churn"] = df["Churn"].map({"Yes":1,"No":0})
 
-    print(f"Churn:", df["Churn"])
+    print(df["Churn"].value_counts())
 
-    return(df)
+    return df
---
+++ src/eda.py
@@ -0,0 +1,7 @@
+def eda(df):    
+    print(df.head())
+    print(df.info())
+    print(df.describe())
+    print(df.columns)
+     
+    return df
